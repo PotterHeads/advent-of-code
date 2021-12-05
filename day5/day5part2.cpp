4c41903e5e320af67417b3479de6e322dfdef5d4
Nicholas <nicklzw97@hotmail.com>
2021-12-06 00:00:36 +0800

    day5

--- day5/day5part2.cpp
+++ day5/day5part2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     std::ios::sync_with_stdio(false); std::cin.tie(NULL);
-    freopen("in2.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
     vector<vector<int>> AM(1000,vector<int>(1000,0));
     int x1,y1,x2,y2;
     string s;
@@ -32,39 +32,18 @@ int main(){
         }
         else if (abs(x2-x1) == abs(y2-y1)){
             for(int i = 0;i<=abs(y2-y1);i++){
-                // arr[a + i*(c-a)/abs(c-a)][b + i*(d-b)/abs(d-b)]++;
-                int change_x = x1 + i*(x2-x1)/abs(x2-x1);
+                int change_x = x1 + i*(x2-x1)/abs(x2-x1); // x2-x1 determines if its addition or subtraction
                 int change_y = y1 + i*(y2-y1)/abs(y2-y1);
-                cout<<"i "<<i<<" x "<<change_x<<" y "<<change_y<<"\n";
+                AM[change_x][change_y]++;
+                if(AM[change_x][change_y] == 2){
+                    ans++;
+                }
             }
         }
     }
-    // cout<<ans<<"\n";
+    cout<<ans<<"\n";
     return 0;
 }
 
  
-            // int change_x = abs(x2-x1);
-            // int change_y = abs(y2-y1);
-            // if(change_x == change_y){
-            //     int start_x = min(x1,x2);
-            //     int end_x = max(x1,x2);
-            //     int start_y = min(y1,y2);
-            //     int end_y = max(y1,y2);
-            //     for(int i = start_x;i<=end_x;i++){
-            //         for(int j = start_y;j<=end_y;j++){
-            //             int changex = abs(i-x1);
-            //             int changey = abs(j-y1);
-            //             if(changex == changey){
-            //                 AM[i][j]++;
-            //                 if(AM[i][j] == 2){
-            //                     ans++;
-            //                 }                            
-            //             }
-            //             // AM[i][j]++;
-            //             // if(AM[i][j] == 2){
-            //             //     ans++;
-            //             // }
-            //         }
-            //     }
-            // }
+  
