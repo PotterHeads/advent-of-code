2b7ec7111b49e51a373478f88301699ffc7c82a3
Nicholas <nicklzw97@hotmail.com>
2021-12-16 23:26:55 +0800

    day16

---
+++ day16/day16part1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    std::ios::sync_with_stdio(false); std::cin.tie(NULL);
+    freopen("in2.txt", "r", stdin);
+    
+    return 0;
+}
+//100010100000000001001010100000000001101010000000000000101111010001111000
+//
