96b6740ce9048168f9a01f149f93f1e1d5fb1c5e
Nicholas <nicklzw97@hotmail.com>
2021-12-17 23:41:37 +0800

    day16

--- day16/day16part1.cpp
+++ day16/day16part1.cpp
@@ -1,69 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string name;
+
+// immediately edit the string to remove unecessary bits 
 string parse (string &binary,long long &version){
-    long long idx = 0;
     string packet_version = binary.substr(0,3);
     version+=stoll(packet_version,0,2);
-    cout<<packet_version<<"\n";
-    cout<<"version "<<version<<"\n";
-    string packet_id= binary.substr(3,3);
+    binary = binary.substr(3);
+    string packet_id= binary.substr(0,3);
     long long id = stoll(packet_id,0,2);
-    idx+=6;
+    binary = binary.substr(3);
     if(id == 4){
-        char bit = binary[6];
-        while(bit!='0'){
-            idx+=5;
-            bit = binary[idx];
+        string num;
+        while(true){
+            num+=binary.substr(0,5);
+            char bit = binary[0];
+            binary = binary.substr(5);
+            if(bit == '0'){
+                break;
+            }
         }
-        idx+=5;
-        string new_str = binary.substr(idx);
-        binary = new_str;
     }
     else{
-        char bit = binary[6];
+        char bit = binary[0];
+        binary = binary.substr(1);
         if(bit == '0'){
-            // cout<<"HERE"<<"\n";
-            // cout<<"BINARY SDFSD654 "<<binary<<"\n";
-            string length = binary.substr(7,15);
-            // cout<<"BINARRSDFASD SD "<<length<<"\n";
+            string length = binary.substr(0,15);
+            binary = binary.substr(15);
             long long size = stoll(length,0,2);
-            // cout<<"SIZE DSF "<<size<<"\n";
-            string new_binary = binary.substr(22,size);
-            // cout<<"HERE 2345 "<<new_binary<<"\n";
-            // cout<<"HERE? "<<new_binary.size()<<"\n";
+            string new_binary = binary.substr(0,size);
             while(new_binary.size() != 0){
-                // cout<<"HERE OR THERE? "<<new_binary.size()<<"\n";
-                // cout<<new_binary<<"\n";
                 new_binary = parse(new_binary,version);
             }
-            string new_str = binary.substr(22+size);
-            binary = new_str;
+            binary = binary.substr(size);
         }
-        // if(bit == '1'){
         else{
-            string length = binary.substr(7,11);
-            // cout<<"LENGTH "<<length<<"\n";
+            string length = binary.substr(0,11);
+            binary = binary.substr(11);
             long long size = stoll(length,0,2);
-            // cout<<"SIZE "<<size<<"\n";
-            for(int i = 0;i<size;i++){
-                string new_str = binary.substr(18);
-                // cout<<"NEW STR "<<new_str<<"\n";
-                binary = parse(new_str,version);
+            for(long long i = 0;i<size;i++){
+                binary = parse(binary,version);
             }
-            // return parse(binary,version);
         }
     }
-    // cout<<"PARSE FUNC "<<binary<<"\n";
     return binary;
-    // return parse(binary,version); 
 }
 
 int main(){
     std::ios::sync_with_stdio(false); std::cin.tie(NULL);
-    freopen("in2.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
     long long version = 0;
-    int i = 0;
     unordered_map<char,string> um = {
         {'0',"0000"},
         {'1', "0001"},
@@ -86,9 +73,7 @@ int main(){
     for(int i = 0;i<line.size();i++){
         binary+=um[line[i]];
     }
-    cout<<"BINARY VALUE FIRST "<<binary<<"\n";
     parse(binary,version);
-    cout<<"AFTER "<<binary<<"\n";
     cout<<"VERSION VALUE "<<version<<"\n";
     return 0;
 }
