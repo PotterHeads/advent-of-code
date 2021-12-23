7c4658a70dba5858d5f3d17b1d4ff9fafc9a95d5
Nicholas <nicklzw97@hotmail.com>
2021-12-24 00:31:30 +0800

    day21

--- day21/day21part1.cpp
+++ day21/day21part1.cpp
@@ -15,42 +15,46 @@ int main(){
     long long ans = 0;
     int n = 3;
     while(true){
-        int curr_p1 = (3*n)-3;
-        int p1_value = curr_p1 + p1_pos;
-        if(p1_value > 10){
-            p1_value %=10;
+        int p1_roll_value = (3*n)-3;
+        int p1_val = p1_roll_value + p1_pos;
+        if(p1_val > 10 or p1_val == 0){
+            p1_val = p1_val%10;
+            if(p1_val = 0){
+                p1_val = 1;
+            }
         }
-        // cout<<"P1 VALUE "<<p1_value<<"\n";
-        p1score += p1_value;
-        p1_pos =  p1_value;
+        p1score += p1_val;
+        p1_pos = p1_val;
+        count+=3;
+        n+=3;
+        cout<<"P1 ROLL VALUE "<<p1_roll_value<<" "<<p1score<<" "<<p1_pos<<"\n";
         if(p1score >= 1000){
             break;
         }
-        // cout<<"P1 SCORE "<<p1score<<"\n";
-        // cout<<"P1 POS "<<p1_pos<<"\n";
-        n+=3;
-        count +=3;
-        int curr_p2 = (3*n)-3;
-        int p2_value = curr_p2 + p2_pos;
-        if(p2_value > 10){
-            p2_value%=10;
+        int p2_roll_value = (3*n)-3;
+        int p2_val = p2_roll_value + p2_pos;
+        if(p2_val > 10 or p2_val == 0){
+            p2_val = p2_val%10;
+            if(p2_val == 0){
+                p2_val = 1;
+            }
         }
-        p2score += p2_value;
-        p2_pos =  p2_value;
-        count+=3;
+        p2score += p2_val;
+        p2_pos = p2_val;
+        cout<<"P2 ROLL VALUE "<<p2_roll_value<<" "<<p2score<<" "<<p2_pos<<"\n";         
         n+=3;
+        count+=3;
         if(p2score >= 1000){
             break;
         }
-        // cout<<"P2 SCORE "<<p2score<<"\n";
-        // cout<<"P2 POS "<<p2_pos<<"\n";
-        // break;
     }
     if(p1score < 1000){
         cout<<"P1 "<<p1score<<" "<<count<<"\n";
+        cout<<"P2 "<<p2score<<" "<<count<<"\n";
         ans = p1score*count;
     }
     else{
+        cout<<"P1 "<<p1score<<" "<<count<<"\n";
         cout<<"P2 "<<p2score<<" "<<count<<"\n";
         ans = p2score*count;
     }
