b68771fa3b6053c65e42d40c7942b7a5eb108a77
Nicholas <nicklzw97@hotmail.com>
2021-12-24 00:48:31 +0800

    day21

--- day21/day21part1.cpp
+++ day21/day21part1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     std::ios::sync_with_stdio(false); std::cin.tie(NULL);
-    freopen("in2.txt", "r", stdin);
+    freopen("in.txt", "r", stdin);
     string p1,p2,p1pos,p2pos;getline(cin,p1);getline(cin,p2);
     p1pos = p1.substr(p1.find(':')+2);
     p2pos = p2.substr(p2.find(':')+2);
@@ -15,33 +15,27 @@ int main(){
     long long ans = 0;
     int n = 3;
     while(true){
-        int p1_roll_value = (3*n)-3;
-        int p1_val = p1_roll_value + p1_pos;
-        if(p1_val > 10 or p1_val == 0){
-            p1_val = p1_val%10;
-            if(p1_val = 0){
-                p1_val = 1;
-            }
+        int p1_roll_value = ((3*n)-3) %10;
+        int p1_sum = p1_roll_value + p1_pos;
+        if(p1_sum > 10){
+            p1_sum%=10;
         }
-        p1score += p1_val;
-        p1_pos = p1_val;
+        p1score += p1_sum;
+        p1_pos = p1_sum;
         count+=3;
         n+=3;
-        cout<<"P1 ROLL VALUE "<<p1_roll_value<<" "<<p1score<<" "<<p1_pos<<"\n";
+        // cout<<"P1 ROLL VALUE "<<p1_roll_value<<" "<<p1score<<" "<<p1_pos<<"\n";
         if(p1score >= 1000){
             break;
         }
-        int p2_roll_value = (3*n)-3;
-        int p2_val = p2_roll_value + p2_pos;
-        if(p2_val > 10 or p2_val == 0){
-            p2_val = p2_val%10;
-            if(p2_val == 0){
-                p2_val = 1;
-            }
+        int p2_roll_value = ((3*n)-3) %10;
+        int p2_sum = p2_roll_value + p2_pos;
+        if(p2_sum > 10){
+            p2_sum%=10;
         }
-        p2score += p2_val;
-        p2_pos = p2_val;
-        cout<<"P2 ROLL VALUE "<<p2_roll_value<<" "<<p2score<<" "<<p2_pos<<"\n";         
+        p2score += p2_sum;
+        p2_pos = p2_sum;
+        // cout<<"P2 ROLL VALUE "<<p2_roll_value<<" "<<p2score<<" "<<p2_pos<<"\n";         
         n+=3;
         count+=3;
         if(p2score >= 1000){
@@ -49,13 +43,13 @@ int main(){
         }
     }
     if(p1score < 1000){
-        cout<<"P1 "<<p1score<<" "<<count<<"\n";
-        cout<<"P2 "<<p2score<<" "<<count<<"\n";
+        // cout<<"P1 "<<p1score<<" "<<count<<"\n";
+        // cout<<"P2 "<<p2score<<" "<<count<<"\n";
         ans = p1score*count;
     }
     else{
-        cout<<"P1 "<<p1score<<" "<<count<<"\n";
-        cout<<"P2 "<<p2score<<" "<<count<<"\n";
+        // cout<<"P1 "<<p1score<<" "<<count<<"\n";
+        // cout<<"P2 "<<p2score<<" "<<count<<"\n";
         ans = p2score*count;
     }
     cout<<ans<<"\n";
